71bc616482cb5015036765d8d805a530bc950c5b
Durli <hojayevadurli@gmail.com>
2023-04-24 18:29:27 -0600

    added login razor page

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SessionFinal.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
