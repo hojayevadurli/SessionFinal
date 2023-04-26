da4007af23ecdf45422e8fdec25f4e34ca6eeca7
Durli <hojayevadurli@gmail.com>
2023-04-26 11:39:21 -0600

    creating the account

--- Pages/CreateAccount.cshtml.cs
+++ Pages/CreateAccount.cshtml.cs
@@ -1,18 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace SessionFinal.Pages
 {
     public class CreateAccountModel : PageModel
     {
-        public void OnGet()
+        private readonly UserContext userContext;
+
+        public string Code { get; set; }
+        public string Email { get; private set; }
+
+        public CreateAccountModel(string code, UserContext userContext )
         {
+            Code = code;
+            this.userContext = userContext;
+           
         }
 
-        public void OnPost(string password)
+       
+        public async Task<IActionResult> OnGet(string code)
         {
+            var codeRow=await userContext.SignupCodes.FirstOrDefaultAsync(x => x.Code == code);
+            if (codeRow == null)
+            {
+                return RedirectToPage("/signup", new { message = "Invalid code" });
+                
+            }
+            else if(codeRow.ExpiresOn<DateTime.Now){
+                return RedirectToPage("/signup", new { message = "Expired Code" });
+            }
+            else
+            {
+                Code = code;
+                Email = codeRow.Email;
+                return Page();
+            }
+        }
 
-        
+        public async Task<IActionResult> OnPost(string password, string code)
+        {
+            var hashedPassword=PasswordHasher.HashPasword(password,out var salt);
+            var accountCode= await userContext.SignupCodes.FirstOrDefaultAsync(c=>c.Code==Code);
+            if (accountCode == null)
+            {
+                return RedirectToPage("/signup", new { message = "Invalid code" });
+            }
+            else if (accountCode.ExpiresOn < DateTime.Now)
+            {
+                return RedirectToPage("/signup", new { message = "Expired Code" });
+            }
+
+            var userAccount = new User
+            {
+                Email = accountCode.Email,
+                HashedPassword = hashedPassword,
+                Id = accountCode.Id
+            };
+            userContext.Users.Add(userAccount);
+            await userContext.SaveChangesAsync();
+
+            return Page();
         }
 
 
--- UserContext.cs
+++ UserContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SessionFinal
 {
@@ -37,6 +39,34 @@ namespace SessionFinal
         public DateTime ExpiresOn { get; set; }
         public string Email { get; set; }
        
+    }
+
+    public static class PasswordHasher
+    {
+        const int keySize = 64;
+        const int iterations = 350000;
+        static HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+        public static string HashPasword(string password, out byte[] salt)
+        {
+            salt = RandomNumberGenerator.GetBytes(keySize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(password),
+                salt,
+                iterations,
+                hashAlgorithm,
+                keySize);
+            return Convert.ToHexString(hash);
+        }
+        public static string HashPasword(string password, string salt)
+        {
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                Encoding.UTF8.GetBytes(password),
+                Convert.FromHexString(salt),
+                iterations,
+                hashAlgorithm,
+                keySize);
+            return Convert.ToHexString(hash);
+        }
     }
 
 }
