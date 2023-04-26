1faad27068f14df7cc9c2ad29fff180d2cc4a5b6
Durli <hojayevadurli@gmail.com>
2023-04-26 10:43:46 -0600

    created create account page

---
+++ Pages/CreateAccount.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SessionFinal.Pages
+{
+    public class CreateAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost(string password)
+        {
+
+        
+        }
+
+
+    }
+}
